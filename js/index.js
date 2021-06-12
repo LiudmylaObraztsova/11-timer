4cce16e09450942c290247386e92f1ad1c2deae2
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-06-12 23:47:06 +0300

    Исправляет фильтр

--- js/index.js
+++ js/index.js
@@ -5,61 +5,59 @@ const refs = {
   secs: document.querySelector('[data-value="secs"]'),
 };
 
-
 class CountdownTimer {
-  constructor({selector, targetDate}) {
-    this.selector = selector;
+  constructor({ selector, targetDate }) {
+    this.selector = document.querySelector(selector);
     this.targetDate = targetDate;
+    this.intervalId = null;
   }
-
-  start = setInterval(() => {
-    const nowDate = Date.now();
-    const remainingTime = this.targetDate - nowDate;
-    const time = this.getTimeComponents(remainingTime);
-
-    this.updateClockFace(time);
-    this.stop(time)
-  }, 1000);
-
-  stop(time) {
-    if (time <= 0) {
-      clearInterval(this.start)
-      refs.timer.textContent = 'Happy Birthday'
-    }
-  }
-
 
   getTimeComponents(time) {
-   
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-    
     const hours = this.pad(
       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     );
-
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-    return { days, hours, mins, secs };
-  }
-
-
-  pad(value) {
-    return String(value).padStart(2, "0");
-  }
-
-
-  updateClockFace({ days, hours, mins, secs }) {
     refs.days.textContent = `${days}`;
     refs.hours.textContent = `${hours}`;
     refs.mins.textContent = `${mins}`;
     refs.secs.textContent = `${secs}`;
   }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  start() {
+    refs.days.textContent = "00";
+    refs.hours.textContent = "00";
+    refs.mins.textContent = "00";
+    refs.secs.textContent = "00";
+  }
+
+  renderTimer() {
+    const startTime = Date.now();
+    const deltaTime = this.targetDate - startTime;
+    this.getTimeComponents(deltaTime);
+    if (deltaTime < 0) {
+      clearInterval();
+      this.start();
+    }
+  }
+
+  initTimer() {
+    this.renderTimer();
+    this.intervalId = setInterval(() => {
+      this.renderTimer();
+    }, 1000);
+  }
 }
 
-// Новый таймер
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Oct 18, 2021'),
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Oct 18, 2021"),
 });
+
+timer.initTimer();
